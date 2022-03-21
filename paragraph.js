e029929bc53a292dff84eab8683887080c77d513
ryuheng <101920652+ryuheng@users.noreply.github.com>
2022-03-21 10:12:46 +0800

    Update paragraph.js

--- paragraph.js
+++ paragraph.js
@@ -6,15 +6,7 @@ const paragraph = [
     "I just wanna tell you how I'm feeling.",
     "Gotta make you understand.",
     "Never gonna give you up.",
-    "Never gonna let you down.",
-    "A cough is a talk from the right perspective.",
-    "A designed tractor's tray comes with it the thought that the snuffly flax is a rainbow.",
-    "Their health was, in this moment, an earthy passbook.",
-    "This could be, or perhaps the swordfishes could be said to resemble healthy sessions.",
-    "A capricorn is a helium from the right perspective.",
-    "However, a sled is a mailman's tennis.",
-    "The competitor of an alarm becomes a toeless raincoat.",
-    "Their twist was, in this moment."
+    "Never gonna let you down."
 ]
 
 const sentences = paragraph.map(sentence => sentence);
